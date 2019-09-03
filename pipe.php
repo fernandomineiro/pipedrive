bb87c787c90593dffcbc213da8feede9b466989b
fernandomineiro <fernandofitilan@hotmail.com>
2019-09-03 14:24:32 -0300

    Update pipe.php

--- pipe.php
+++ pipe.php
@@ -1,20 +1,26 @@
 <?php
 
+$url = 'https://api.pipedrive.com/v1/persons?start=0&api_token=51c907f748371ece6722554ddc028fb30ad87098';
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.pipedrive.com/v1/persons?start=0&api_token=51c907f748371ece6722554ddc028fb30ad87098",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_POSTFIELDS => "",
-  CURLOPT_HTTPHEADER => array(
-    "Postman-Token: 0a84957f-4edc-407f-a3ff-65d319f6b0ba",
-    "cache-control: no-cache"
-  ),
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+    CURLOPT_HTTPHEADER => array(
+        "Accept: application/json",
+        'Content-type' => 'application/json',
+        "Accept-Encoding: gzip, deflate",
+        "Connection: keep-alive",
+        "Host: api.pipedrive.com",
+        "authorization: Token 51c907f748371ece6722554ddc028fb30ad87098",
+        "cache-control: no-cache",
+        "content-type: application/json"
+    ),
 ));
 
 $response = curl_exec($curl);
@@ -23,16 +29,12 @@ $err = curl_error($curl);
 curl_close($curl);
 
 if ($err) {
-  echo "cURL Error #:" . $err;
+    echo "cURL Error #:" . $err;
 } else {
     $response = json_decode($response, true);
     $data = $response["data"];
-    foreach($data as $p)
-    {
-        //return $p;
-        $name = $p['phone']['value'];
-        
-        echo "<p><b>Nome:</b> ".$name."</p>";
-        
+    foreach($data as $p){
+    $res = $p['phone']['value'];
+    echo "<b>Telefone: </b>". $res;
     }
 }
